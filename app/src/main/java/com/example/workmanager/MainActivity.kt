ff0957b30bd97e5b1f2e910f08989589e97aea41
Gilang <gilangnurrizki173@gmail.com>
2024-10-07 10:14:11 +0700

    periodic logic

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -1,26 +1,27 @@
 package com.example.workmanager
 
+import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.work.Constraints
 import androidx.work.Data
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.example.workmanager.databinding.ActivityMainBinding
-import android.Manifest
-import androidx.work.Constraints
-import androidx.work.NetworkType
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var workManager: WorkManager
+    private lateinit var periodicWorkRequest: PeriodicWorkRequest
     private lateinit var binding: ActivityMainBinding
 
     private val requestPermissionLauncher =
@@ -43,14 +44,47 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         }
 
         workManager = WorkManager.getInstance(this)
+
         binding.btnOneTimeTask.setOnClickListener(this)
+        binding.btnPeriodicTask.setOnClickListener(this)
+        binding.btnCancelTask.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btnOneTimeTask -> startOneTimeTask()
+            R.id.btnPeriodicTask -> startPeriodicTask()
+            R.id.btnCancelTask -> cancelPeriodicTask()
         }
+    }
+
+    private fun cancelPeriodicTask() {
+        workManager.cancelWorkById(periodicWorkRequest.id)
+    }
+
+    private fun startPeriodicTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        periodicWorkRequest = PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES)
+            .setInputData(data)
+            .setConstraints(constraints)
+            .build()
+        workManager.enqueue(periodicWorkRequest)
+        workManager.getWorkInfoByIdLiveData(periodicWorkRequest.id)
+            .observe(this@MainActivity) { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+                binding.btnCancelTask.isEnabled = false
+                if (workInfo.state == WorkInfo.State.ENQUEUED) {
+                    binding.btnCancelTask.isEnabled = true
+                }
+            }
     }
 
     private fun startOneTimeTask() {
